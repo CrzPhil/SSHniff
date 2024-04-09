1ec7450f1b41d1d5a551a9738dca78c30aacb625
CrzPhil <philippos.g@me.com>
2024-04-09 13:05:58 +0000

    output formatting

--- sshniff/src/ui/output.rs
+++ sshniff/src/ui/output.rs
@@ -23,21 +23,47 @@ fn print_core(session: &SshSession) {
     let line = "\u{2500}";
     println!("\u{2503} Stream {}", Colour::Red.paint(session.stream.to_string()));
 
-    println!("\u{2503}");
-    print!("\u{2503}\u{256D}");
-    println!("{:\u{2500}^40}\u{256E}", "Client");
-    println!("\u{2503}\u{2502}{:^40}\u{2502}", &session.src);
-    println!("\u{2503}\u{2502}{:^40}\u{2502}", &session.hassh_c);
-    println!("\u{2503}\u{2502}{:^40}\u{2502}", &session.protocols.0);
-    println!("\u{2503}\u{2570}{}\u{256F}", line.repeat(40));
+    // Stacked:
+    
+//    println!("\u{2503}");
+//    print!("\u{2503}\u{256D}");
+//    println!("{:\u{2500}^40}\u{256E}", "Client");
+//    println!("\u{2503}\u{2502}{:^40}\u{2502}", &session.src);
+//    println!("\u{2503}\u{2502}{:^40}\u{2502}", &session.hassh_c);
+//    println!("\u{2503}\u{2502}{:^40}\u{2502}", &session.protocols.0);
+//    println!("\u{2503}\u{2570}{}\u{256F}", line.repeat(40));
+//
+//    print!("\u{2503}\u{256D}");
+//    println!("{:\u{2500}^40}\u{256E}", "Server");
+//    println!("\u{2503}\u{2502}{:^40}\u{2502}", &session.dst);
+//    println!("\u{2503}\u{2502}{:^40}\u{2502}", &session.hassh_s);
+//    println!("\u{2503}\u{2502}{:^40}\u{2502}", &session.protocols.1);
+//    println!("\u{2503}\u{2570}{}\u{256F}", line.repeat(40));
 
-    print!("\u{2503}\u{256D}");
+    // Horizontal:
+
+    // === Row 1 ===
+    print!("\u{2503}{}", Colour::Green.paint("\u{256D}"));
+    print!("{}", Colour::Green.paint(format!("{:\u{2500}^40}\u{256E}", "Client")));
+    print!("      ");
+    print!("\u{256D}");
     println!("{:\u{2500}^40}\u{256E}", "Server");
-    println!("\u{2503}\u{2502}{:^40}\u{2502}", &session.dst);
-    println!("\u{2503}\u{2502}{:^40}\u{2502}", &session.hassh_s);
-    println!("\u{2503}\u{2502}{:^40}\u{2502}", &session.protocols.1);
-    println!("\u{2503}\u{2570}{}\u{256F}", line.repeat(40));
-
+    // === Row 2 === 
+    print!("\u{2503}{}", Colour::Green.paint(format!("\u{2502}{:^40}\u{2502}", &session.src)));
+    print!("      ");
+    println!("\u{2502}{:^40}\u{2502}", &session.dst);
+    // === Row 3 ===
+    print!("\u{2503}{}", Colour::Green.paint(format!("\u{2502}{:^40}\u{2502}", &session.hassh_c)));
+    print!("{}", Colour::Green.paint("----->"));
+    println!("\u{2502}{:^40}\u{2502}", &session.hassh_s);
+    // === Row 4 ===
+    print!("\u{2503}{}", Colour::Green.paint(format!("\u{2502}{:^40}\u{2502}", &session.protocols.0)));
+    print!("      ");
+    println!("\u{2502}{:^40}\u{2502}", &session.protocols.1);
+    // === Row 5 ===
+    print!("\u{2503}{}", Colour::Green.paint(format!("\u{2570}{}\u{256F}", line.repeat(40))));
+    print!("      ");
+    println!("\u{2570}{}\u{256F}", line.repeat(40));
 
 //    println!("\u{2503} Client      : {:<24} - {}", Colour::Blue.paint(&session.src), Colour::Fixed(226).paint(&session.protocols.0));
 //    println!("\u{2503} hassh       : {}", Colour::Fixed(226).paint(&session.hassh_c));
